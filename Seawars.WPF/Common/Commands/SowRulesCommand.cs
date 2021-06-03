7fe3fc58c39ed63c64ddd82af867c28aead2f2f5
Urbanovskiy Anton <antonurbanovskiy461@gmail.com>
2021-06-03 19:10:04 +0300

    Rules window

---
+++ Seawars.WPF/Common/Commands/SowRulesCommand.cs
@@ -0,0 +1,15 @@
+﻿using Seawars.WPF.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seawars.WPF.Common.Commands
+{
+    class SowRulesCommand : BaseCommand
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => new RulesWindow().ShowDialog();
+    }
+}
