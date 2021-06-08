5ed05c2c2f51962092cfef18b19629ebb32fbf34
Urbanovskiy Anton <antonurbanovskiy461@gmail.com>
2021-06-08 22:39:47 +0300

    Web Api settings

--- Seawars.WebApi/Startup.cs
+++ Seawars.WebApi/Startup.cs
@@ -1,12 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Seawars.DAL.Context;
-using Microsoft.AspNetCore.Builder;
 
 namespace Seawars.WebApi
 {
@@ -14,12 +10,7 @@ namespace Seawars.WebApi
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Seawars.WebApi", Version = "v1" });
-            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,8 +18,6 @@ namespace Seawars.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seawars.WebApi v1"));
             }
 
             app.UseHttpsRedirection();
