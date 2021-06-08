cea03d22e4287b47310a2d795eba630bd3f7777a
Urbanovskiy Anton <antonurbanovskiy461@gmail.com>
2021-06-08 23:22:36 +0300

    Interface for clients connection

---
+++ Seawars.Interfaces/Services/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seawars.Interfaces.Services
+{
+    public interface IConnection
+    {
+        string CreateGame();
+        string JoinGame(string Id);
+    }
+}
---
+++ Seawars.WebApi.Clients/Connection/GameConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seawars.Interfaces.Services;
+
+namespace Seawars.WebApi.Clients.Connection
+{
+    public class GameConnection : IConnection
+    {
+        public string CreateGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string JoinGame(string Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
